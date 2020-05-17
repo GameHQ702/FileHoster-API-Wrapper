96590519acd57760cd683b3111b4695a6f725a1a
GameHQ702 <tutorialhq702@gmail.com>
2020-05-17 22:17:52 +0200

    updated

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # FileHoster API Wrapper
- This is a simple File Hoster API Wrappe in Python
+
+Its a simple API Wrapper for Python
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install foobar
+```
+
+## Supported / Unsupported Hoster
+
+- [ ] Mediafire
+- [x] Anonfile
+- [ ] Mega.nz
+- [x] Vidoza
+- [x] Vivo
+
+
+## Usage
+
+```python
+import main as FileHoster
+
+vivo = FileHoster.Vivo(vivotoken)
+accinfo = vivo.AccInfo()
+print(accinfo)
+```
--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-import main as FileUploader
+import main as FileHoster
 import json
 
 config = json.load(open('config.json'))
 
 
-#vivo = FileUploader.Vivo(vivotoken)
+#vivo = FileHoster.Vivo(vivotoken)
 #GetEmbed = vivo.GetEmbed('https://vivo.sx/9c23ee2be2')
 #fileinfo = vivo.FileInfo('https://vivo.sx/9c23ee2be2')
 #accinfo = vivo.AccInfo()
 
-#anon = FileUploader.Anon(anontoken)
+#anon = FileHoster.Anon(anontoken)
 #fileinfo = anon.FileInfo('x4u6z206o0') fileinfo[0] = name, fileinfo[1] = size
 #upload = anon.Upload('video.mp4')
 
-#vidoza = FileUploader.Vidoza(vidozatoken)
+#vidoza = FileHoster.Vidoza(vidozatoken)
 #upload = vidoza.AdvancedUpload('video.mp4', 'Test Title', 'Test Desc', 0, 3, 'Test Tag')
 #info = vidoza.FileInfo('https://vidoza.net/hcpsm5x91nwq.html')
 
